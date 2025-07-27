194cc55776f0eba6c9d9ef9d82069d5169afb830
Chuthuka <chuthuka097dedunu@gmail.com>
2025-07-27 10:24:28 +0530

    Create Repositories

---
+++ src/main/java/com/cpd/hotel_system/hotel_management_service_api/repository/FileFormatterRepo.java
@@ -0,0 +1,8 @@
+package com.cpd.hotel_system.hotel_management_service_api.repository;
+
+import com.cpd.hotel_system.hotel_management_service_api.entity.FileFormatter;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FileFormatterRepo extends JpaRepository<FileFormatter,String> {
+
+}
