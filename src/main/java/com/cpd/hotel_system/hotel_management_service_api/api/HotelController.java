12fa6adc1b78f77147dca911b04694cb0b8aad70
Chuthuka <chuthuka097dedunu@gmail.com>
2025-08-06 20:16:44 +0530

    Create UPDATE controll

--- src/main/java/com/cpd/hotel_system/hotel_management_service_api/api/HotelController.java
+++ src/main/java/com/cpd/hotel_system/hotel_management_service_api/api/HotelController.java
@@ -1,10 +1,37 @@
 package com.cpd.hotel_system.hotel_management_service_api.api;
 
+import com.cpd.hotel_system.hotel_management_service_api.dto.request.RequestHotelDto;
+import com.cpd.hotel_system.hotel_management_service_api.dto.response.ResponseHotelDto;
+import com.cpd.hotel_system.hotel_management_service_api.service.HotelService;
+import com.cpd.hotel_system.hotel_management_service_api.util.StandardResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hotel-management/api/v1/hotels")
 public class HotelController {
+    private final HotelService hotelService;
+
+    public HotelController(HotelService hotelService) {
+        this.hotelService = hotelService;
+    }
+
+    @PostMapping("/user/create")
+    public ResponseEntity<StandardResponseDto> create(
+            @RequestBody RequestHotelDto dto){
+             hotelService.create(dto);
+             return new ResponseEntity<>(
+                     new StandardResponseDto(
+                             201,"Hotel Saved!!",null
+                     ),
+                     HttpStatus.CREATED
+             );
+
+
+    }
 
 }
