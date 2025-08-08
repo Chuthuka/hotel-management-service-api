d5155493293516a1baa236e4e486b5ae4584505f
Chuthuka <chuthuka097dedunu@gmail.com>
2025-08-08 08:26:26 +0530

    Create Delete,FindById,FindAll Implementations

--- src/main/java/com/cpd/hotel_system/hotel_management_service_api/api/HotelController.java
+++ src/main/java/com/cpd/hotel_system/hotel_management_service_api/api/HotelController.java
@@ -6,10 +6,7 @@ import com.cpd.hotel_system.hotel_management_service_api.service.HotelService;
 import com.cpd.hotel_system.hotel_management_service_api.util.StandardResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hotel-management/api/v1/hotels")
@@ -30,8 +27,58 @@ public class HotelController {
                      ),
                      HttpStatus.CREATED
              );
-
-
     }
 
+
+    @PutMapping("/admin/update/{id}")
+    public ResponseEntity<StandardResponseDto> update(
+            @PathVariable ("id") String hotelId,
+            @RequestBody RequestHotelDto dto){
+        hotelService.update(dto,hotelId);
+        return new ResponseEntity<>(
+                new StandardResponseDto(
+                        201,"Hotel Updated!!",null
+                ),
+                HttpStatus.CREATED
+        );
+    }
+
+
+    @DeleteMapping("/host/delete/{id}")
+    public ResponseEntity<StandardResponseDto> delete(
+            @PathVariable ("id") String hotelId) {
+        hotelService.delete(hotelId);
+        return new ResponseEntity<>(
+                new StandardResponseDto(
+                        204, "Hotel Deleted!!", null
+                ),
+                HttpStatus.NO_CONTENT
+        );
+    }
+
+
+    @GetMapping("/visitor/find-by-id/{id}")
+    public ResponseEntity<StandardResponseDto> findById(
+            @PathVariable ("id") String hotelId) {
+        return new ResponseEntity<>(
+                new StandardResponseDto(
+                        200, "Hotel Found!!", hotelService.findById(hotelId)
+                ),
+                HttpStatus.OK
+        );
+    }
+
+
+    @GetMapping("/visitor/find-all")
+    public ResponseEntity<StandardResponseDto> findAll(
+            @RequestParam String searchText,
+            @RequestParam int page,
+            @RequestParam int size){
+        return new ResponseEntity<>(
+                new StandardResponseDto(
+                        200, "Hotel List!!", hotelService.findAll(page,size,searchText)
+                ),
+                HttpStatus.OK
+        );
+    }
 }
